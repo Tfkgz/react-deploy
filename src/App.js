31d85a2c40254617eb18e4df42bb414070c093ad
Tfkgz <tawfik.ghazi@gmail.com>
2022-08-05 00:30:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import mylogo from './doors_02.jpg';
+
+function App() {
+  return (
+    <div className="App">   
+        Hello
+        <br></br>
+        <img src={mylogo} alt="logo" width="250"/>  
+    </div>
+  );
+}
+
+export default App;
